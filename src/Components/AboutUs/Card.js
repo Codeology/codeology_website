c59a4f0f49d1b981c3990db65edfcddef90bfefd
Amy Huang <amyxjhuang@berkeley.edu>
2021-03-29 00:05:50 -0700

    figured out overlay

--- src/Components/AboutUs/Card.js
+++ src/Components/AboutUs/Card.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Popup from "reactjs-popup";
-import './css/Card.css'
+import './css/Card.css';
 
 class Card extends React.Component {
     render() {
       var name_arr = this.props.name.split(this.props.name);
         return (
-          <Popup trigger={
-              <div className="cardSheet">
+          <Popup modal trigger={
+            <div className="cardSheet">
               {/* Card info  */}
                 <img className="cardImg" src={this.props.imgSrc}></img>
                       <div className="cardName">{this.props.name}</div>
                         <p className="cardRole">{this.props.role}</p>
                       </div>}
+
                  position="top center">
 
             {/* Profile popup content */}
@@ -21,13 +22,12 @@ class Card extends React.Component {
               <div className="profile-name"> {name_arr[0]} </div>
               <img className="profile-img" src={this.props.imgSrc}/>
 
-              <div className="profile-category">Year: </div>
-              <div className="profile-category">Role: </div>
-              <div className="profile-category">Email: </div>
-              <div className="profile-category">Tree: </div>
-              <div className="profile-category">Leaf: </div>
+              <div className="profile-category">Year: Freshman </div>
+              <div className="profile-category">Role: {this.props.role}</div>
+              <div className="profile-category">Email: email@berkeley.edu </div>
+              <div className="profile-category">Tree: Your mom</div>
+              <div className="profile-category">Leaf: Your dad</div>
               <div className="profile-category">Links: </div>
-
 
             </div>
           </Popup>
