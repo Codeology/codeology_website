b993b734f704b3fd24aad6e36ac6147c1c7ae0a5
Ashley Chu <ashleymchu@yahoo.com>
2021-03-30 10:34:28 -0700

    added footer currently

--- src/Components/Home/Footer.js
+++ src/Components/Home/Footer.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import './Footer.css'
 
 function Footer() {
     return (
         <footer className="Footer">
+          <div class="FooterSection1">
+            <div className="Logo">
+              <img src={require("./assets/images/logo.png")}/>
+              <h1>Codeology</h1>
+            </div>
+            <p>University of California, Berkeley</p>
+          </div>
+          <div className="FooterSection">
+            <h2>General</h2>
+            <ul>
+              <li>About Us</li>
+              <li>Spring 2021 Projects</li>
+              <li>Apply</li>
+            </ul>
+          </div>
+          <div className="FooterSection2">
+            <h2>Contact</h2>
+            <ul>
+              <li>info@codeology.club</li>
+              <li>Contact Us</li>
+            </ul>
             <a href="https://www.instagram.com/berkeleycodeology/" target="_blank">
                 <img src={require("./assets/images/instagram.png")} />
             </a>
@@ -12,6 +34,7 @@ function Footer() {
             <a href="https://www.facebook.com/berkeleycodeology/" target="_blank">
                 <img src={require("./assets/images/facebook.png" )}/>
             </a>
+          </div>
         </footer>
     )
 }
