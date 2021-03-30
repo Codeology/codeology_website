d27569eb7aeb1efd22fda9352c175d782d308da5
Amy Huang <amyxjhuang@berkeley.edu>
2021-03-30 16:10:39 -0700

    profile popup (fixed categories section and photo)

--- src/Components/AboutUs/Card.js
+++ src/Components/AboutUs/Card.js
@@ -5,6 +5,7 @@ import './css/Card.css';
 class Card extends React.Component {
     render() {
       var name_arr = this.props.name.split(this.props.name);
+
         return (
           <Popup modal trigger={
             <div className="cardSheet">
@@ -18,17 +19,26 @@ class Card extends React.Component {
 
             {/* Profile popup content */}
             <div className="profile-background">
-              <div className="profile-name" id="first-name"> {this.props.name}</div>
-              <div className="profile-name"> {name_arr[0]} </div>
-              <img className="profile-img" src={this.props.imgSrc}/>
+            <div className="profile-name first-name"> {this.props.name}</div>
+            <div className="profile-name"> {name_arr[0]} </div>
+            <img className="profile-img" src={this.props.imgSrc}/>
 
-              <div className="profile-category">Year: Freshman </div>
-              <div className="profile-category">Role: {this.props.role}</div>
-              <div className="profile-category">Email: email@berkeley.edu </div>
-              <div className="profile-category">Tree: Your mom</div>
-              <div className="profile-category">Leaf: Your dad</div>
-              <div className="profile-category">Links: </div>
+              <p className="profile-text">
+                <span className="profile-category">Year:</span> Freshman <br/>
+                <span className="profile-category">Role:</span> {this.props.role}<br/>
+                <span className="profile-category">Email:</span> email@berkeley.eduf<br/>
+                <span className="profile-category">Tree:</span> Your mom <br/>
+                <span className="profile-category">Leaf:</span> Your dad <br/>
+                <span className="profile-category">Links:</span>
+                  <a className="profile-link linkedin">linkedin</a>
+                  <a className="profile-link resume">resume</a>
+                  <a className="profile-link website">website</a>
 
+                <br/>
+
+                <div className="profile-blurb"> Hi this is the blurb sadlfkjasld lkasdjf lkasdjf lksadj ldskaj </div>
+
+              </p>
             </div>
           </Popup>
 
