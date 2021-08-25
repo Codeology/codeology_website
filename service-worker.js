5dd201b282aa1edf68929757c615afdec3df867e
Amy Huang <amyxjhuang@berkeley.edu>
2021-08-24 18:37:20 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cc5edcc00ad7beccec78803b1a0fbb9.js"
+  "/precache-manifest.a8cebbf9105331d19fa15fbbb9be54a8.js"
 );
 
 self.addEventListener('message', (event) => {
