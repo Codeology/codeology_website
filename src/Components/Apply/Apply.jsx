0aae1deb8700b8b63dd171ca1b626675c83a36fd
Amy Huang <amyxjhuang@berkeley.edu>
2021-08-29 21:39:16 -0700

    member page edits + infosession stuff

--- src/Components/Apply/Apply.jsx
+++ src/Components/Apply/Apply.jsx
@@ -12,7 +12,21 @@ export default function Apply() {
                 <img className="applyImages" src={require("./images/flyer.png")} alt="flyer"/>
                 <img className="applyImages" src={require("./images/info.png")} alt="info"/>
                 <div id="apply-subtext">
-                    <p>Application deadline is 9/3 at 11:59PM. Come join us at one of our infosessions (8-9:30PM) to learn more about projects and our club!</p>
+                    <p>Application deadline is 9/3 at 11:59PM. Come join us at one of our infosessions (8-9:30PM) to learn more about projects and our club!
+                    </p>
+                        
+                    <p>INFOSESSION 1 - HYBRID <br/>
+                        Tuesday 8/31, 8:00 - 9:30 PM PDT<br/>
+                        Social Sciences Buildling (Barrows Hall) 56 <br/>
+
+                        Zoom Link: <a href="https://berkeley.zoom.us/my/sreevidyaganga">https://berkeley.zoom.us/my/sreevidyaganga</a>
+                        <br/><br/>
+                        INFOSESSION 2 - HYBRID<br/>
+                        Thursday 9/2, 8:00 - 9:30 PM PDT<br/>
+                        Genetics and Plant Biology Building 100<br/>
+
+                        Zoom Link:  <a href="https://berkeley.zoom.us/my/sreevidyaganga">https://berkeley.zoom.us/my/sreevidyaganga</a>
+                    </p>
                 </div>
                 <div id="bottombox">
 
